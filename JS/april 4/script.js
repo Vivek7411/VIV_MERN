3bc401d5786b60ff90dcd3a1c2a02bbb17e52580
VIVEK NEHRA <143152527+Vivek7411@users.noreply.github.com>
2024-04-04 20:41:22 +0530

    Add files via upload

---
+++ JS/april 4/script.js
@@ -0,0 +1,94 @@
+
+// var message = 'hello world';
+// var age =18;
+// var age ="Done";
+// console.log(message);
+
+//console.log(message);
+// function env(){
+    
+    
+    // var message ='hello';
+    
+    // console.log(message);
+    
+    // var message='else';
+    
+    // console.log(message);
+
+// env();
+
+// let message='hello';
+// message='else';
+// console.log(message);
+
+
+// const message='hello';
+// // message='else'; // not allowed. 
+// console.log(message);
+
+// let arr=['a', 'b']
+// console.log(arr);
+
+// let a = 12;
+// let b = '12';
+
+// if(a == b){
+//     console.log('YES');
+// }else{
+//     console.log('NO');
+// }
+
+
+// if(a === b){
+//     console.log('YES');
+// }else{
+//     console.log('NO');
+// }
+
+// print();
+// function print(){
+//     consol.log('--');
+// }
+
+// const a = function print(){
+//     consol.log('--');
+
+
+// }
+
+// const b = function print(){
+//     consol.log('--');
+
+    
+// }
+
+// print();
+// a();
+// b();
+
+// let message='hello';
+// message='else';
+// console.log(message);
+
+
+// const message='hello';
+// // message='else'; // not allowed. 
+// console.log(message);
+
+// let arr=['a', 'b']
+// console.log(arr);
+
+// let a = 12;
+// let b = '12';
+
+// if(a == b){
+//     console.log('YES');
+// }else{
+//     console.log('NO');
+// }
+
+
+const x = prompt('value da dho pls');
+console.log(x);
+
