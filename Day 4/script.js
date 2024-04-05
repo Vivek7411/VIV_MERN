f68a07cb265117970e6056b9658d11d1278d9321
Vivek7411 <vivnehra74@gmail.com>
2024-04-05 20:00:53 +0530

    day 4 work

---
+++ Day 4/script.js
@@ -0,0 +1,106 @@
+// let a=new String("Hello");
+// let b="hello";
+// if(a==b)
+// {
+
+//     console.log('yes');
+// }
+// else
+// {
+//     console.log('no');
+// }
+
+// const obj = {
+//     name:"ajay",
+//     lastname:"sing"
+// }
+
+// console.log(obj);
+// document.write(obj);
+
+
+// let arr = [1,2,3,4];
+// arr=[2,3,4]
+// console.log(arr);
+// console.log(Array.isArray(arr));
+
+// function isObject(arg) {
+//     return typeof arg === 'object' && arg !== null;
+//   }
+  
+
+//   console.log(isObject({}));  
+//   console.log(isObject([])); 
+//   console.log(isObject(null)); 
+//   console.log(isObject(42));   
+//   console.log(isObject("test"));
+  
+
+// let text = "";
+
+// for (let i = 0; i < 5; i++) {
+//   text += "The number is " + i + "<br>";
+// }
+
+// document.getElementById("demo").innerHTML = text;
+
+
+// console.log(window.document)
+// console.log(window)
+// console.dir(window.document)
+
+// const res =
+// document.getElementById('t1)
+// console.dir(res)
+
+
+
+
+// let objectArray = [];
+
+
+// function addObject(obj) {
+//     objectArray.push(obj);
+// }
+
+
+// function removeObjectByIndex(index) {
+//     if (index >= 0 && index < objectArray.length) {
+//         objectArray.splice(index, 1);
+//     } else {
+//         console.error("Index out of bounds");
+//     }
+// }
+
+
+// let obj1 = { name: "Object 1", value: 10 };
+// let obj2 = { name: "Object 2", value: 20 };
+
+
+// addObject(obj1);
+// addObject(obj2);
+
+// console.log("Objects in array:", objectArray);
+
+
+// removeObjectByIndex(0);
+
+// console.log("Objects in array after removal:", objectArray);
+
+// const res = document.querySelector('div');
+
+// // console.log(res);
+// // res.remove();
+
+// const c = document.querySelector('p');
+// res.removeChild(c);
+
+// const parent = document.getElementById("parent");
+// const child = document.getElementById("child");
+// const throwawayNode = parent.removeChild(child);
+
+// function displayDate() {
+//     document.getElementById("demo").innerHTML = Date();
+
+// }
+
