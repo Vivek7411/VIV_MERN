d3f4ea88b6a0f7016382141a49afa86a55a2bb39
Vivek7411 <vivnehra74@gmail.com>
2024-04-15 10:07:59 +0530

    modified

--- Day 7/internal_modules/script.js
+++ Day 7/internal_modules/script.js
@@ -1,41 +1,82 @@
-// const fs = require('node:fs');
+// // const fs = require('node:fs');
 
-// const data = fs.readFileSync('./myReadMe.txt',{encoding:'utf8'});
+// // const data = fs.readFileSync('./myReadMe.txt',{encoding:'utf8'});
 
-// console.log(data);
-
-
-// const fs = require('fs');
-// fs.writeFileSync('./logs.txt',"10th April 2024 Day 7");
+// // console.log(data);
 
 
-
-// const fs = require('fs');
-// console.log('start');
-// const data = fs.readFileSync('./myReadMe.txt',{encoding:'utf8'});
-// console.log(data);
-// console.log('End');
-
-//promisis
-// const fsPromises = require('fs/promises');
-// console.log('start');
-// const pr = fsPromises.readFile('./myReadMe.txt',{encoding: 'utf8'});
-// console.log(pr);
-// pr.then((res)=>{
-//     console.log(res);
-// })
+// // const fs = require('fs');
+// // fs.writeFileSync('./logs.txt',"10th April 2024 Day 7");
 
 
-//callback
-// const fs = require('fs');
 
-// fs.readFile('./myReadMe.txt',{encoding: "utf8"}, (err,data)=>{
-//     console.log(data);
-// });
+// // const fs = require('fs');
+// // console.log('start');
+// // const data = fs.readFileSync('./myReadMe.txt',{encoding:'utf8'});
+// // console.log(data);
+// // console.log('End');
+
+// //promisis
+// // const fsPromises = require('fs/promises');
+// // console.log('start');
+// // const pr = fsPromises.readFile('./myReadMe.txt',{encoding: 'utf8'});
+// // console.log(pr);
+// // pr.then((res)=>{
+// //     console.log(res);
+// // })
 
 
-//http://localhost:1400/
+// //callback
+// // const fs = require('fs');
+
+// // fs.readFile('./myReadMe.txt',{encoding: "utf8"}, (err,data)=>{
+// //     console.log(data);
+// // });
+
+
+// //http://localhost:1400/
+// // const http = require('http');
+
+// // const app = http.createServer((req, res)=>{
+// //     console.log('received');
+// //     console.log(req.url);
+// //     res.writeHead(200,{
+// //         'Content-type':'text/html',
+// //     })
+// //     res.end('<h2>Hello</h2><br><h3>Hii</h3>');
+    
+// // });
+
+// // app.listen(1400, ()=>{
+// //     console.log('..........Server Started........');
+// // });     //1000<port<9998
+
+
+
 // const http = require('http');
+
+// const htmlTemplate = `
+// <!DOCUMENT HTML>
+// <html>
+//     <head>
+
+//     </head>
+//     <body>
+//         _PRODUCT__CARDS_
+//     </body>
+// </html>
+// `
+
+// const cardTemplate = `
+// <div class = 'product-card'>
+// <h4>__TITLLE</h4>
+// <p>__INFO__</p>
+// </div>
+// `
+// // const card1 = htmlTemplate.replace('')
+
+// const page = htmlTemplate.replace('_PRODUCT__CARDS_',cardTemplate);
+
 
 // const app = http.createServer((req, res)=>{
 //     console.log('received');
@@ -43,7 +84,7 @@
 //     res.writeHead(200,{
 //         'Content-type':'text/html',
 //     })
-//     res.end('<h2>Hello</h2><br><h3>Hii</h3>');
+//     res.end(page);
     
 // });
 
@@ -51,45 +92,66 @@
 //     console.log('..........Server Started........');
 // });     //1000<port<9998
 
+// // jebfwjebf
 
 
 const http = require('http');
+const fs = require('fs');
+const data = fs.readFileSync('./data.json', 'utf8');
+const dataObj = JSON.parse(data);
+const products = dataObj.products;
 
 const htmlTemplate = `
-<!DOCUMENT HTML>
+<!DOCTYPE HTML>
 <html>
-    <head>
-
-    </head>
-    <body>
-        _PRODUCT__CARDS_
-    </body>
+<head>
+    <title>Product List</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        product
+    </div>
+</body>
 </html>
-`
+`;
 
 const cardTemplate = `
-<div class = 'product-card'>
-<h4>__TITLLE</h4>
-<p>__INFO__</p>
+<div class='product-card'>
+    <h4>_TITLE</h4>
+    <p>_INFO</p>
 </div>
-`
-// const card1 = htmlTemplate.replace('')
+`;
 
-const page = htmlTemplate.replace('_PRODUCT__CARDS_',cardTemplate);
-
-
-const app = http.createServer((req, res)=>{
-    console.log('received');
-    console.log(req.url);
-    res.writeHead(200,{
-        'Content-type':'text/html',
-    })
-    res.end(page);
-    
+const allCards = products.map((elem) => {
+    let newCard = cardTemplate;
+    newCard = newCard.replace('_TITLE', elem.title);
+    newCard = newCard.replace('_INFO', elem.description);
+   
+    return newCard;
 });
 
-app.listen(1400, ()=>{
-    console.log('..........Server Started........');
-});     //1000<port<9998
+const allCardsString = allCards.join(' ');
 
-// jebfwjebf
+const page = htmlTemplate.replace('product', allCardsString);
+
+const app = http.createServer((req, res) => {
+    console.log('received');
+    console.log(req.url);
+    if (req.url === '/styles.css') {
+        const cssFile = fs.readFileSync('./styles.css', 'utf8');
+        res.writeHead(200, { 'Content-type': 'text/css' });
+        res.write(cssFile);
+        res.end();
+    } else {
+        res.writeHead(200, {
+            'Content-type': 'text/html',
+        });
+        res.end(page);
+    }
+});
+
+app.listen(1400, () => {
+    console.log("-----------server started----------------");
+});
+
